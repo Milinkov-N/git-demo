dd305d867072607aeeb56b6f91e2b5bb10d15c8d
Milinkov-N <milinkov.nik@gmail.com>
2022-10-03 22:20:13 +0400

    feat(bin): impl remaining operations

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,37 @@ fn main() {
 
     match operation {
         1 => {
-            print!("Enter first number: ");
-            io::stdout().flush().unwrap();
-            let x: i32 = input::number();
-
-            print!("Enter second number: ");
-            io::stdout().flush().unwrap();
-            let y: i32 = input::number();
+            let (x, y) = numbers_input();
 
             println!("{x} + {y} = {}", arith::add(x, y));
         }
-        2 => todo!(),
-        3 => todo!(),
-        4 => todo!(),
+        2 => {
+            let (x, y) = numbers_input();
+
+            println!("{x} - {y} = {}", arith::sub(x, y));
+        }
+        3 => {
+            let (x, y) = numbers_input();
+
+            println!("{x} * {y} = {}", arith::mul(x, y));
+        }
+        4 => {
+            let (x, y) = numbers_input();
+
+            println!("{x} / {y} = {}", arith::div(x, y));
+        }
         op => unreachable!("Unknown operation \"{op}\""),
     }
+}
+
+fn numbers_input() -> (i32, i32) {
+    print!("Enter first number: ");
+    io::stdout().flush().unwrap();
+    let x: i32 = input::number();
+
+    print!("Enter second number: ");
+    io::stdout().flush().unwrap();
+    let y: i32 = input::number();
+
+    (x, y)
 }
