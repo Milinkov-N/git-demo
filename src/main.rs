a6c1622f39f17e40dc6a368fd65e4027dc959ce3
Milinkov Nikita <73058993+Milinkov-N@users.noreply.github.com>
2022-10-03 22:47:48 +0400

    Release/v1.0.0
    * feat(lib): added number method to input mod
    
    * test: added basic tests to number function
    
    * feat(lib): added line method
    
    * refactor: made use of new input mod
    
    * fix: removed newline chars from return string in line method
    
    * feat(lib): added sub, mul and div methods
    
    * feat(bin): added selection of operations
    
    * feat(bin): impl remaining operations
    
    * docs: added 1.0.0 to changelog
    
    * refactor: match operation

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,59 @@
 use std::io::{self, Write};
 
-use git_demo::arithmetic as arith;
+use git_demo::{arithmetic as arith, input};
+
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
 
 fn main() {
-    let mut input = String::new();
-    let mut user_name = String::new();
-
     print!("Enter your name: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut user_name).unwrap();
 
-    println!("Hello, {}!", user_name.trim());
+    println!("Hello, {}!", input::line());
 
+    println!(
+        "Choose operation:{}{}{}{}",
+        "\n  1. Summation", "\n  2. Subdivision", "\n  3. Multiplication", "\n  4. Division"
+    );
+
+    print!("> ");
+    io::stdout().flush().unwrap();
+    let operation: i32 = input::number();
+
+    match operation {
+        1 => handle_operation(Op::Add),
+        2 => handle_operation(Op::Sub),
+        3 => handle_operation(Op::Mul),
+        4 => handle_operation(Op::Div),
+        op => unreachable!("Unknown operation \"{op}\""),
+    }
+}
+
+fn numbers_input() -> (i32, i32) {
     print!("Enter first number: ");
     io::stdout().flush().unwrap();
-
-    io::stdin().read_line(&mut input).unwrap();
-    let x = input.trim().parse().unwrap_or(0);
-    input.clear();
+    let x: i32 = input::number();
 
     print!("Enter second number: ");
     io::stdout().flush().unwrap();
+    let y: i32 = input::number();
 
-    io::stdin().read_line(&mut input).unwrap();
-    let y = input.trim().parse().unwrap_or(0);
+    (x, y)
+}
 
-    println!("{x} + {y} = {}", arith::add(x, y));
+fn handle_operation(operation: Op) {
+    let (x, y) = numbers_input();
+
+    let result = match operation {
+        Op::Add => arith::add(x, y),
+        Op::Sub => arith::sub(x, y),
+        Op::Mul => arith::mul(x, y),
+        Op::Div => arith::div(x, y),
+    };
+
+    println!("{x} * {y} = {result}");
 }
