0ac7884ca4d3cdb0c0b23a2bab4487ffe1affd2a
Milinkov Nikita <73058993+Milinkov-N@users.noreply.github.com>
2022-10-04 14:01:22 +0400

    feat: changed input api and added new trait (#6)
    * feat: changed input api and added new trait
    
    * fix(bin): removed input unwraps

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+use std::ops::{Add, Div, Mul, Rem, Sub};
+
 pub mod arithmetic;
 pub mod input;
+
+pub trait Number<Rhs = Self, Output = Self>:
+    Add<Rhs, Output = Self>
+    + Div<Rhs, Output = Self>
+    + Sub<Rhs, Output = Self>
+    + Mul<Rhs, Output = Self>
+    + Rem<Rhs, Output = Self>
+    + Sized
+{
+}
+
+impl<T, Rhs, Output> Number<Rhs, Output> for T where
+    T: Add<Rhs, Output = Self>
+        + Div<Rhs, Output = Self>
+        + Sub<Rhs, Output = Self>
+        + Mul<Rhs, Output = Self>
+        + Rem<Rhs, Output = Self>
+{
+}
