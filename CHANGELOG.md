d9e76769b0498f7bca20040c1cf6656a0011ba03
Milinkov Nikita <73058993+Milinkov-N@users.noreply.github.com>
2022-10-04 17:15:32 +0400

    Release/v1.1.0 (#9)
    * feat(lib): added number method to input mod
    
    * test: added basic tests to number function
    
    * feat(lib): added line method
    
    * refactor: made use of new input mod
    
    * fix: removed newline chars from return string in line method
    
    * feat(lib): added sub, mul and div methods
    
    * feat(bin): added selection of operations
    
    * feat(bin): impl remaining operations
    
    * docs: added 1.0.0 to changelog
    
    * refactor: match operation
    
    * feat: changed input api and added new trait (#6)
    
    * feat: changed input api and added new trait
    
    * fix(bin): removed input unwraps
    
    * fix: removed all unwraps
    
    * fix: removed all unwraps from main.rs (#7)
    
    * feat(lib): changed methods to be generic over numbers
    
    * update: changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# 1.1.0 (2022-10-04)
+
+## Release Highlights
+
+- Arithmetic and input functions for number now generic over all number types via `Number` trait
+
+- No panics when running program
+
+- More robust and mantainable code
+
 # 1.0.0 (2022-10-03)
 
 ## Features
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
 
 use git_demo::{arithmetic as arith, input};
 
@@ -10,50 +13,72 @@ enum Op {
 }
 
 fn main() {
-    print!("Enter your name: ");
-    io::stdout().flush().unwrap();
+    print_flush("Enter your name: ");
 
-    println!("Hello, {}!", input::line());
+    match input::line() {
+        Ok(name) => println!("Hello, {name}!"),
+        Err(e) => {
+            eprintln!("Error: {e}");
+            exit(1);
+        }
+    }
 
     println!(
         "Choose operation:{}{}{}{}",
         "\n  1. Summation", "\n  2. Subdivision", "\n  3. Multiplication", "\n  4. Division"
     );
 
-    print!("> ");
-    io::stdout().flush().unwrap();
-    let operation: i32 = input::number();
+    print_flush("> ");
+    let operation = input::number();
 
-    match operation {
+    if let Err(e) = operation {
+        eprintln!("Error: {e}");
+        exit(1);
+    }
+
+    match operation.unwrap() {
         1 => handle_operation(Op::Add),
         2 => handle_operation(Op::Sub),
         3 => handle_operation(Op::Mul),
         4 => handle_operation(Op::Div),
-        op => unreachable!("Unknown operation \"{op}\""),
+        op => eprintln!("Unknown operation \"{op}\""),
     }
 }
 
-fn numbers_input() -> (i32, i32) {
-    print!("Enter first number: ");
-    io::stdout().flush().unwrap();
-    let x: i32 = input::number();
+fn print_flush(prompt: &str) {
+    print!("{prompt}");
 
-    print!("Enter second number: ");
-    io::stdout().flush().unwrap();
-    let y: i32 = input::number();
+    if let Err(e) = io::stdout().flush() {
+        eprintln!("Error: {e}");
+        exit(1);
+    }
+}
 
-    (x, y)
+fn numbers_input() -> Result<(i32, i32), input::InvalidInput> {
+    print_flush("Enter first number: ");
+    let x: i32 = input::number()?;
+
+    print_flush("Enter second number: ");
+    let y: i32 = input::number()?;
+
+    Ok((x, y))
 }
 
 fn handle_operation(operation: Op) {
-    let (x, y) = numbers_input();
+    match numbers_input() {
+        Ok((x, y)) => {
+            let result = match operation {
+                Op::Add => arith::add(x, y),
+                Op::Sub => arith::sub(x, y),
+                Op::Mul => arith::mul(x, y),
+                Op::Div => arith::div(x, y),
+            };
 
-    let result = match operation {
-        Op::Add => arith::add(x, y),
-        Op::Sub => arith::sub(x, y),
-        Op::Mul => arith::mul(x, y),
-        Op::Div => arith::div(x, y),
-    };
-
-    println!("{x} * {y} = {result}");
+            println!("{x} * {y} = {result}");
+        }
+        Err(e) => {
+            eprintln!("Error: {e}");
+            exit(1);
+        }
+    }
 }
